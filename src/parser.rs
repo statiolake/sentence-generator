2d1c1a5d628a0fcd5b62fa4bfe68c62a4784a566
statiolake <statiolake@gmail.com>
2019-02-06 15:12:51 +0900

    add parse error type

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,39 @@
+use std::error;
+use std::fmt;
+use std::result;
 use std::str::Chars;
 
 use crate::ast::*;
+
+#[derive(Debug)]
+pub struct FilePos {
+    linum: u32,
+}
+
+impl fmt::Display for FilePos {
+    fn fmt(&self, b: &mut fmt::Formatter) -> fmt::Result {
+        write!(b, "in line {}", self.linum)
+    }
+}
+
+pub type Result<T> = result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    UnexpectedToken { pos: FilePos, found: String },
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, b: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::UnexpectedToken { ref pos, ref found } => {
+                write!(b, "{}: unexpected token {}", pos, found)
+            }
+        }
+    }
+}
+
+impl error::Error for Error {}
 
 pub struct Parser<'a> {
     chars: Chars<'a>,
@@ -13,31 +46,31 @@ impl<'a> Parser<'a> {
         }
     }
 
-    pub fn parse(&mut self) -> Ast {
-        Ast {
-            program: self.parse_program(),
-        }
+    pub fn parse(&mut self) -> Result<Ast> {
+        Ok(Ast {
+            program: self.parse_program()?,
+        })
     }
 }
 
 impl<'a> Parser<'a> {
-    fn parse_program(&mut self) -> Program {
+    fn parse_program(&mut self) -> Result<Program> {
         unimplemented!()
     }
 
-    fn parse_rule(&mut self) -> Rule {
+    fn parse_rule(&mut self) -> Result<Rule> {
         unimplemented!()
     }
 
-    fn parse_sentence(&mut self) -> Sentence {
+    fn parse_sentence(&mut self) -> Result<Sentence> {
         unimplemented!()
     }
 
-    fn parse_item(&mut self) -> Item {
+    fn parse_item(&mut self) -> Result<Item> {
         unimplemented!()
     }
 
-    fn parse_expr(&mut self) -> Expr {
+    fn parse_expr(&mut self) -> Result<Expr> {
         unimplemented!()
     }
 }
