3ac13fd7efdb40b9b6ac811107740f18bdcf7c72
statiolake <statiolake@gmail.com>
2019-02-07 17:30:40 +0900

    support generate multiple sentences

--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,19 @@ fn main() {
 fn logic_main() -> Result<(), Box<dyn error::Error>> {
     let mut rng = rand::thread_rng();
 
-    let file_name = env::args()
-        .nth(1)
-        .unwrap_or_else(|| "index.txt".to_string());
+    let mut args = env::args().skip(1);
+
+    let file_name = args.next().unwrap_or_else(|| "index.txt".to_string());
+    let count: usize = args.next().unwrap_or_else(|| "1".to_string()).parse()?;
 
     let mut source = String::new();
     File::open(&file_name)?.read_to_string(&mut source)?;
     let mut parser = parser::Parser::new(&source);
-
     let ast = parser.parse()?;
 
-    println!("{}", ast.generate(&mut rng)?);
+    for _ in 0..count {
+        println!("{}", ast.generate(&mut rng)?);
+    }
 
     Ok(())
 }
